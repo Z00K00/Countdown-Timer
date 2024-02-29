eeb28b5ffd8b097c2760a9adbf2f69162ab85f10
Z00K00 <kraigalarson@gmail.com>
2024-02-28 20:03:09 -0500

    Initial Commit

---
+++ myFirstRealApp/myFirstRealAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirstRealAppApp.swift
+//  myFirstRealApp
+//
+//  Created by Krajg Larson on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct myFirstRealAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
