aca69c5c5f7dff2eab662b70e5175a2f51e10894
Kraig Larson <147437185+Z00K00@users.noreply.github.com>
2024-02-28 20:34:15 -0500

    added countdown

--- myFirstRealApp/ContentView.swift
+++ myFirstRealApp/ContentView.swift
@@ -8,16 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    let secondTimer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State var countdown: Int = 180
+    @State var isPaused = true
+    
+    var seconds: Int {
+        secondsToHoursMinutesSeconds(countdown).seconds
+    }    
+    
+    var minutes: Int {
+        secondsToHoursMinutesSeconds(countdown).minutes
+    }
+    
+    var hours: Int {
+        secondsToHoursMinutesSeconds(countdown).hours
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Text("\(hours) Hours")
+                Text("\(minutes) Minutes")
+                Text("\(seconds) Seconds")
+            }
+            
+            if isPaused {
+                Button("Start Countdown") {
+                    print("Start Countdown")
+                    resetCountdown()
+                    isPaused = false
+                }
+            } else {
+                Button("Pause Countdown") {
+                    print("Pause Countdown")
+                    isPaused = true
+                }
+            }
         }
-        .padding()
+        .onReceive(secondTimer, perform: { _ in
+//            print("Update Countdown")
+            if !isPaused {
+                countdown -= 1
+            }
+        })
+    }
+    
+    func secondsToHoursMinutesSeconds(_ seconds: Int) -> (hours: Int, minutes: Int, seconds: Int) {
+        return (seconds / 3600, (seconds % 3600) / 60, (seconds % 3600) % 60)
+    }
+    
+    func resetCountdown() {
+        countdown = 180
     }
 }
+
+//struct CountdownDetails {}
 
 #Preview {
     ContentView()
